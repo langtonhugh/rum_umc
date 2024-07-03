cbe7fccb61ee41bb9063a88d47ed10b898a58902
langtonhugh <s.h.langton@amsterdamumc.nl>
2024-07-03 11:17:36 +0200

    demo

--- session_material/scripts/session_2_demo.R
+++ session_material/scripts/session_2_demo.R
@@ -9,6 +9,48 @@ tt_df <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday
 # Explore.
 glimpse(tt_df)
 
+# Plot basic scatterplot, between billboard-spotify pop.
+ggplot(data = tt_df,
+       mapping = aes(x = weeks_on_billboard,
+                     y = spotify_popularity)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)
+
+# Same plot in a different way.
+ggplot(data = tt_df) +
+  geom_point(mapping = aes(x = weeks_on_billboard,
+                           y = spotify_popularity)) +
+  geom_smooth(mapping = aes(x = weeks_on_billboard,
+                            y = spotify_popularity))
+
+# Make more complex.
+ggplot(data = tt_df,
+       mapping = aes(x = weeks_on_billboard,
+                     y = spotify_popularity,
+                     colour = release_year)) +
+  geom_point() +
+  labs(x = "Billboard rank", y = "Spotify pop.",
+       title = "Relationship between billboard & spotify",
+       subtitle = "Just a demo",
+       caption = "Data from tidytuesday") +
+  scale_colour_viridis_c()
+
+# Boxplot.
+ggplot(data = tt_df) +
+  geom_boxplot(mapping = aes(x = ave_age_at_top_500,
+                             # y = artist_gender,
+                             fill = artist_gender)) +
+  scale_fill_brewer(palette = "Set2") +
+  labs(x = "whatever it might be",
+       fill = "Gender")
+
+
+
+
+
+
+
+
 
 
 
