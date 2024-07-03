b059e7302fd60c80fd4bc7ac6fa2ebb50df8307e
langtonhugh <s.h.langton@amsterdamumc.nl>
2024-07-03 09:53:50 +0200

    ggplot2 stuff

---
+++ session_material/scripts/session_2_demo.R
@@ -0,0 +1,12 @@
+# Load packages.
+library(readr)
+library(dplyr)
+library(ggplot2)
+
+# Extract the data.
+tt_df <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-05-07/rolling_stone.csv')
+
+# Explore.
+glimpse(tt_df)
+
+
