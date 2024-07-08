b3fa5bb5ab688d60af9189b725e2a65e9999f1f8
langtonhugh <s.h.langton@amsterdamumc.nl>
2024-07-08 13:36:58 +0200

    session 3 prep script

--- session_material/scripts/session_3.R
+++ session_material/scripts/session_3.R
@@ -1,8 +1,8 @@
 # Load packages.
 library(readr)
 library(dplyr)
-library(cbsdataR)
 library(sf)
+library(patchwork)
 library(ggplot2)
 
 # Load in tidytuesday data (manually).
@@ -48,5 +48,49 @@ places_sighting_nl_sf <- places_nl_sf %>%
 ggplot(data = places_sighting_nl_sf) +
   geom_sf(mapping = aes(colour = n))
 
+# Load in regional spatial data.
+regio_sf <- st_read("https://github.com/langtonhugh/rum_umc/raw/main/session_material/data/geo/B1_Provinciegrenzen_van_Nederland.gpkg")
 
+# Joint plot.
+ggplot() +
+  geom_sf(data = regio_sf) +
+  geom_sf(data = places_sighting_nl_sf)
+
+# Spatial join.
+regio_ufo_sf <- regio_sf %>% 
+  st_join(places_sighting_nl_sf)
+
+# Aggregate.
+regio_ufo_agg_df <- regio_ufo_sf %>% 
+  group_by(state) %>% 
+  summarize(n_sightings = sum(n))
+
+# Regional plot.
+regional_gg <- ggplot(data = regio_ufo_agg_df) +
+  geom_sf(mapping = aes(fill = n_sightings),
+          colour = "black") +
+  scale_fill_viridis_c() +
+  theme_bw() +
+  labs(title = "Regional frequency counts",
+       fill = "Count")
+
+# City plot.
+location_gg <- ggplot() +
+  geom_sf(data = regio_ufo_agg_df) +
+  geom_sf(data = places_sighting_nl_sf) +
+  theme_bw() +
+  labs(title = "Unique sighting locations")
+
+# Arrange. 
+full_plots <- regional_gg + location_gg +
+  plot_annotation(title = "UFO sightings in the Netherlands (1925-2023) \n",
+                  caption = "Source: National UFO Reporting Center | TidyTuesday",
+                  theme = theme(
+                    plot.title = element_text(hjust = 0.5)
+                    )) 
+
+# Save.
+ggsave(plot = full_plots,
+       filename = "visuals/session3/ufo_maps.png",
+       height = 16, width = 22, unit = "cm", dpi = 300)
 
