5483586d940869c0703ffbfb00837c2cd6e3d01f
langtonhugh <s.h.langton@amsterdamumc.nl>
2025-02-12 11:03:38 +0100

    demo script during workshop

--- session_material/scripts/session11_demo.R
+++ session_material/scripts/session11_demo.R
@@ -4,19 +4,50 @@ library(dplyr)
 library(ggplot2)
 library(ggridges)
 
-# Kaggle data csv (free but requires login).
-# https://www.kaggle.com/datasets/hod101s/simpsons-imdb-ratings?resource=download
+# Load in Simpsons data.
 episodes <- read_csv("data/simpsons/SimpsonsData.csv")
 
-# Clean names.
+# Clean the variable names.
 episodes <- episodes %>% 
-  janitor::clean_names()
+  janitor::clean_names() %>% 
+  mutate(season = as.factor(season))
 
-# Initial density plot.
+# Plot basic density plot.
 ggplot(data = episodes) +
-  geom_density(mapping = aes(x = rating, group = season, fill = season))
+  geom_density(mapping = aes(x = rating,
+                             group = season,
+                             fill = season)) +
+  theme(legend.position = "none")
 
-# Ridges density plot.
+# Expand density to a ridges plot.
 ggplot(data = episodes) +
-  geom_density_ridges(mapping )
+  geom_density_ridges(mapping = aes(x = rating,
+                                    y = season,
+                                    fill = season)) +
+  theme_bw() +
+  scale_fill_viridis_d() +
+  theme(legend.position = "none") 
+
+# Make airdate a date.
+episodes <- episodes %>% 
+  mutate(airdate_lub = lubridate::dmy(airdate))
+
+# Make plot of individual episode trend.
+ggplot(data = episodes) +
+  geom_point(mapping = aes(x = airdate_lub, y = rating,
+                           colour = season)) +
+  theme_bw() +
+  scale_colour_viridis_d() +
+  theme(legend.position = "none") +
+  labs(x = "date episode aired")
+
+# Histogram.
+ggplot(data = episodes) +
+  geom_boxplot(mapping = aes(x = season, y = rating,
+                             fill = season)) +
+  theme_bw() +
+  scale_fill_viridis_d() +
+  theme(legend.position = "none")
+
+
 
